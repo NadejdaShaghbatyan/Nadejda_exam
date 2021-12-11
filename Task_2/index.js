04d7f92424ebdb7d2260a0c03dcc702de907cc60
NadejdaShaghbatyan <nadejhda.s.d@tumo.org>
2021-12-11 14:59:50 +0400

    TASK_2

---
+++ Task_2/index.js
@@ -0,0 +1,20 @@
+let persons = [
+    { "first_name": "Armen", "last_name": "Amiryan", "age": "33" },
+    { "first_name": "Karine", "last_name": "Karapetyan", "age": "23" },
+    { "first_name": "Manushak", "last_name": "Voskanyan", "age": "20" },
+    { "first_name": "Eghiazar", "last_name": "Avetisyan", "age": "18" },
+    { "first_name": "Hrant", "last_name": "Tokhatyan", "age": "22" },
+    { "first_name": "Zarzand", "last_name": "Mnatsakanyan", "age": "66" },
+    { "first_name": "Murad", "last_name": "Muradyan", "age": "58" },
+    { "first_name": "Ashkhen", "last_name": "Khanamiryan", "age": "48" }
+]
+let newList =[]
+
+function newPersonList(persons) {
+    for (let i = 0; i < persons.length; i++) {
+        const element = persons[i];
+        newList.push(element.first_name + " " + element.last_name)
+    }  
+    return newList
+}
+console.log( newPersonList(persons))
