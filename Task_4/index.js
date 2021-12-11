47e3e0626fb00a9df5f9797f1c49326ebfae5806
NadejdaShaghbatyan <nadejhda.s.d@tumo.org>
2021-12-11 15:33:11 +0400

    TASK_4

---
+++ Task_4/index.js
@@ -0,0 +1,6 @@
+let names = [ "Mnatsakan", "Karapetyan","Baghdasaryan","Abelyan","Isahakyan"];
+let surnames = ["Hovhannes", "Avetiq", "Daniel","Grigor"];
+
+let list = names.concat(surnames)
+
+console.log(list)
